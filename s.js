a8ee51af7534478ae5e435ed086d0d5096975b78
Simon Darcy <sidarcy@gmail.com>
2017-12-09 16:35:37 +0000

    Added real data and UI updates

--- s.js
+++ s.js
@@ -1,29 +1,60 @@
 var tiles = document.querySelectorAll("#districts g");
 
-
+var cursorX;
+var cursorY;
+document.onmousemove = function(e){
+    cursorX = e.pageX;
+    cursorY = e.pageY;
+}
 
 var chart;
 var currentData;
 $dropdown = $("#dropdown");
 
 
+var currentCount = 0;
+
+    function count(num) {
+
+        var options = {
+            "useEasing": true,
+            "useGrouping": true,
+            "separator": ',',
+            "decimal": '.'
+        };
+        var demo = new CountUp('count', currentCount, num, 0, 2.5, options).start();
+
+        currentCount = num;
+    }
+
+
+
+
+
+
 function renderDropDown(crimes){
 
     $($dropdown).html("");
     $.each(crimes, function(i, crime) {
-        str = "<li data-field='"+i+"'><a href='javascript:void(0);'>"+crime+"</a></li>";
-        $($dropdown).append(str);
+        if (i!="FIELD1" && i!="FIELD11" && i!= "FIELD12"){
+            str = "<li data-field='"+i+"'><a href='javascript:void(0);'>"+crime+"</a></li>";
+            $($dropdown).append(str);
+        }
 
     });
 
     $($dropdown).children("li").on("click", function(){
 
-
-        $('.list-title').text($(this).text());
+        $('#list-heading').text($(this).text());
         $('.listholder').hide();
         updateChart($(this).data("field"));
 
     });
+
+
+    $('.excerpt').text(crimes.FIELD12)
+
+
 
 }
 
@@ -40,7 +71,7 @@ function renderDropDown(crimes){
                     return;
                 }
 
-                renderDropDown(data.Outcome);
+                renderDropDown(data[0]);
                 loadChart(data);
 
 
@@ -48,12 +79,6 @@ function renderDropDown(crimes){
             }
         });
     }
-
-
-
-
-
-
 
 
 
@@ -68,16 +93,24 @@ loadChart = function(blah){
 
 
     data=[];
+    var overallTotal = 0;
 
     $.each(blah, function(i, field) {
 
 
-        if(i!="Outcome"){
-            data.push([i, field.FIELD11])
+        if(i!=0){
+            data.push([field.FIELD1, field.FIELD11])
+            overallTotal+=parseInt(field.FIELD11.replace(",", ''))
         }
 
 
     });
+
+
+    count(overallTotal);
+
+
+
 
 
 
@@ -88,13 +121,13 @@ loadChart = function(blah){
             "type": "donut",
             "onclick": function (d, i) {},
             "onover": function (d, i) {},
-            "onout": function (d, i) {},
+            "onout": function (d, i) {},           
         },
         "legend": {
             "position": "right"
         },
         "donut": {
-            "title": "Outcome"
+           
         },
         "labels": {
             "format": {
@@ -114,19 +147,50 @@ loadChart = function(blah){
 };
 
 
+fields = [
+  "Outcome",
+  "Dismiss",
+  "Disqualification",
+  "Fine",
+  "Impr/Det Suspended",
+  "Imprisonment/Detention",
+  "Other",
+  "Peace Bond",
+  "Probation",
+  "Strike Out",
+  "Taken Into Consideration",
+  ]
+
 updateChart =  function(field){
 
 
-
     this.field = field;
+    this.total=0;
 
     var self = this;
 
+
+
     $.each(currentData, function(i, item) {
-        if(i!="Outcome") {
-            data.push([i, item[self.field]]);
+
+        if(i!=0){
+        
+            
+
+            val = item[self.field];
+
+            if(val==""){
+                val="0"
+            }
+
+            val = parseInt(val)
+            self.total += val
+            data.push([fields[i], val]);
         }
+        
     });
+
+    count(self.total);
 
 
 
@@ -152,11 +216,15 @@ for (var i = tiles.length; i--;) {
         }
         this.setAttribute("class", "selected");
 
-        loadData("district1.json");
+
+        id = this.getAttribute("id")
+
+        loadData("data/"+id+".json");
 
 
-
-        $('.list-title').show().text("Offenses");
+        $('.list-title').show();
+        $('#count').show();
+        $('#list-heading').text("Offenses");
 
         $('#title').text(this.getAttribute("id").replace("district", "District "));
         $('.excerpt').text("We need to put the towns in here")
@@ -164,7 +232,32 @@ for (var i = tiles.length; i--;) {
 
     });
 
+
+    tooltip = document.getElementById("tooltip")
+
+    me.addEventListener("mouseover", function(el){
+
+        tooltip.style.display = "block";        
+        tooltip.style.left = cursorX-50 + 'px';
+        tooltip.style.top = cursorY-90 + 'px';
+
+        tooltip.innerHTML = this.getAttribute("id")
+
+    });
+
 }
+
+
+
+
+var map = document.querySelectorAll('svg')[0];
+
+ map.addEventListener("mouseout", function(el){
+
+        document.getElementById("tooltip").style.display = "none";
+    
+
+    });
 
 
 
